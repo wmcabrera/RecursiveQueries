3f7fb7cc1b959b37c0fc07db6994e3f70eb98f24
Wellington M. Cabrera <wcabrera@gmail.com>
2016-09-21 01:43:48 -0500

    Create List_CSV.java

---
+++ generator/List_CSV.java
@@ -0,0 +1,22 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
+public class List_CSV {
+
+        public static void main(String[] args) throws Exception {
+                int number_Of_Vertices=1000;
+                int cost=1;
+                FileWriter fileWriter = new FileWriter("E:\\datasets\\List\\ListDataSet-1kEdge.csv");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+       
+                for(int i=2;i<=number_Of_Vertices;i++) {
+                        printWriter.println((i-1)+","+i+","+cost);
+                }
+
+                printWriter.flush();
+                printWriter.close();
+                fileWriter.close();
+        }
+
+}
