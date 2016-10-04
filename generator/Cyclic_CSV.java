5711f47e7847e0dfbe0d66ea4225326031bdd49b
Wellington M. Cabrera <wcabrera@gmail.com>
2016-10-04 17:04:19 -0500

    Update Cyclic_CSV.java

--- generator/Cyclic_CSV.java
+++ generator/Cyclic_CSV.java
@@ -1,15 +1,17 @@
 public class Cyclic_CSV {
 
          public static void main(String[] args) throws Exception {
-                int number_Of_Vertices=1000;
+                
+                int N = args[N];
+                int number_Of_Vertices=N;
                 int cost=1;
-                FileWriter fileWriter = new FileWriter("E:\\datasets\\List\\ListDataSet-1kEdge.csv");
+                FileWriter fileWriter = new FileWriter( "Cyclic"+ N+".csv");
         PrintWriter printWriter = new PrintWriter(fileWriter);
        
                 for(int i=2;i<=number_Of_Vertices;i++) {
                         printWriter.println((i-1)+","+i+","+cost);
                 }
-
+                printWriter.println(N+","+"1," + cost);
                 printWriter.flush();
                 printWriter.close();
                 fileWriter.close();
