ddfb1e7c494e26854ceb60237ae19847b4bc9f64
Wellington M. Cabrera <wcabrera@gmail.com>
2016-10-04 16:58:58 -0500

    Update Cyclic_CSV.java

--- generator/Cyclic_CSV.java
+++ generator/Cyclic_CSV.java
@@ -1,23 +1,17 @@
 public class Cyclic_CSV {
 
-        public static void main(String[] args) throws Exception {
-                int number_Of_Vertices=100000;
+         public static void main(String[] args) throws Exception {
+                int number_Of_Vertices=1000;
                 int cost=1;
-                FileWriter fileWriter = new FileWriter("E:\\datasets\\cyclic\\CyclicDataSet-200kEdge.csv");
+                FileWriter fileWriter = new FileWriter("E:\\datasets\\List\\ListDataSet-1kEdge.csv");
         PrintWriter printWriter = new PrintWriter(fileWriter);
-        int j,k;
-        for(int i=1;i<=number_Of_Vertices;i++) {
-        
-                while((j=(new Random()).nextInt(number_Of_Vertices)+1)==i);
-                printWriter.println(i+","+j+","+cost);
-        
-                while((k=(new Random()).nextInt(number_Of_Vertices)+1)==i || k==j);
-                printWriter.println(i+","+k+","+cost);
-        
-        }
-        
-        printWriter.flush();
-        printWriter.close();
-        fileWriter.close();
+       
+                for(int i=2;i<=number_Of_Vertices;i++) {
+                        printWriter.println((i-1)+","+i+","+cost);
+                }
+
+                printWriter.flush();
+                printWriter.close();
+                fileWriter.close();
         }
 }
